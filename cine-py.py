fe8a975b184e4ddc61e0a367bfca23c2a6ed3a46
Mariuxi0105 <mj-mariuxi98@outlook.com>
2026-03-03 09:32:31 -0500

    Add files via upload

---
+++ cine-py.py
@@ -0,0 +1,17 @@
+>>> asientos = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+...
+... f = int(input("Ingrese fila (0 a 2): "))
+... c = int(input("Ingrese columna (0 a 3): "))
+...
+... # Validación de disponibilidad
+... if asientos[f][c] == 0:
+...     asientos[f][c] = 1
+...     print("Asiento reservado con éxito.")
+... else:
+...     print("El asiento ya está ocupado.")
+...
+... print("Estado de la sala:")
+... for i in range(3):
+...     for j in range(4):
+...         print(asientos[i][j], end=" ")
+...     print()
